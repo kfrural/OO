ab415705a0dc69ccfb66ead07f65b276130c3b09
Karla C. Ferreira <karlinharural@gmail.com>
2024-01-02 14:29:21 -0300

    Update ex1.java

--- semana1/ex1.java
+++ semana1/ex1.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-public class Maior{
+public class Main{
     public static void main (String[] args){
         Scanner ler =  new Scanner(System.in);
-        int num1;
+        double num1;
         System.out.println("Escreve um numero ai: ");
-        num1 = ler.nextInt();
-        int num2;
+        num1 = ler.nextDouble();
+        double num2;
         System.out.println("escreve outro: ");
-        num2 = ler.nextInt();
+        num2 = ler.nextDouble();
         if(num1 < num2)
             System.out.println("o maior eh: "+ num2);
         else
