ebf45730f74d327817f8341a18c8f9fbda184139
Karla C. Ferreira <karlinharural@gmail.com>
2024-02-26 19:15:31 -0300

    Create Roda.java

---
+++ semana2/ex2/Roda.java
@@ -0,0 +1,42 @@
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author kfrural
+ */
+public class Roda {
+
+    private double raio;
+    private String material;
+    private double peso;
+
+    public Roda() {
+        this.raio = 0.0;
+        this.material = " ";
+        this.peso = 0.0;
+    }
+
+    public void setRaio(double raio) {
+        this.raio = raio;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getRaio() {
+        return this.raio;
+    }
+
+    public String getMaterial() {
+        return this.material;
+    }
+
+    public double getPeso() {
+        return this.peso;
+    }
+}
