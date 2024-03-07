a27ff91e83734ac66643ff3fd88f74e1c59f4ea6
Karla C. Ferreira <karlinharural@gmail.com>
2024-03-07 20:34:35 -0300

    Create main.java

---
+++ semana5/ex2/main.java
@@ -0,0 +1,22 @@
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author kfrural
+ */
+import com.mycompany.mavenproject2.classes.Livro;
+import java.util.Scanner;
+
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+        Livro.nomeBiblioteca = "Biblioteca";
+        Livro l1 = new Livro("Dexter", "Jeff", 321);
+        Livro l2 = new Livro("de frente com o serial killer", "john", 127);
+
+        l2.setNomeBiblioteca("qqcoisa");
+
+        System.out.println(l1.nomeBiblioteca);
+        System.out.println(l2.getNomeBiblioteca());
+    }
+}
