6ece3e2de2154750a7b6a650a7aad9769af90dee
Karla C. Ferreira <karlinharural@gmail.com>
2024-03-10 20:13:40 -0300

    Create main.java

---
+++ semana6/ex1/main.java
@@ -0,0 +1,17 @@
+package com.mycompany.mavenproject3;
+
+import com.mycompany.mavenproject3.classes.Carro;
+
+/**
+ *
+ * @author kfrural
+ */
+public class Mavenproject3 {
+
+    public static void main(String[] args) {
+        Carro c1 = new Carro();
+        c1.fill();
+        
+        c1.imprimir();
+    }
+}
