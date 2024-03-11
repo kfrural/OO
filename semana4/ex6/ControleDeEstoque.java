b001342bad47792ce4b4322bb693192edfe67348
Karla C. Ferreira <karlinharural@gmail.com>
2024-03-11 12:10:20 -0300

    Create ControleDeEstoque.java

---
+++ semana4/ex6/ControleDeEstoque.java
@@ -0,0 +1,52 @@
+package com.mycompany.mavenproject1.classe;
+
+
+/**
+ *
+ * @author kfrural
+ */import java.util.HashMap;
+
+public class ControleDeEstoque {
+    private HashMap<String, Produto> estoque;
+
+    public ControleDeEstoque() {
+        this.estoque = new HashMap<>();
+    }
+
+    public void adicionarProduto(Produto produto) {
+        estoque.put(produto.getCodigo(), produto);
+        System.out.println("Produto adicionado com sucesso!");
+    }
+
+    public void removerProduto(String codigo) {
+        if (estoque.remove(codigo) != null) {
+            System.out.println("Produto removido com sucesso!");
+        } else {
+            System.out.println("Produto não encontrado!");
+        }
+    }
+
+    public void atualizarQuantidade(String codigo, int novaQuantidade) {
+        Produto produto = estoque.get(codigo);
+        if (produto != null) {
+            produto.setQuantidadeEmEstoque(novaQuantidade);
+            System.out.println("Quantidade atualizada com sucesso!");
+        } else {
+            System.out.println("Produto não encontrado!");
+        }
+    }
+
+    public void listarProdutos() {
+        if (estoque.isEmpty()) {
+            System.out.println("O estoque está vazio.");
+        } else {
+            for (Produto produto : estoque.values()) {
+                System.out.println(produto);
+            }
+        }
+    }
+
+    public Produto buscarProduto(String codigo) {
+        return estoque.get(codigo);
+    }
+}
