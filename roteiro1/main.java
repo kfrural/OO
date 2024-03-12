67321d7899596cd418b19537d052db015c7a0b32
Karla C. Ferreira <karlinharural@gmail.com>
2024-03-12 11:56:42 -0300

    Update main.java

--- roteiro1/main.java
+++ roteiro1/main.java
@@ -10,10 +10,12 @@ public class Mavenproject3 {
 
     public static void main(String[] args) {
         
-        Ponto p = new Ponto();
-        Ponto p2 = new Ponto(12,12);
+               Ponto p = new Ponto(4, 40);
+        Ponto p2 = new Ponto(2,2);
+        Ponto p3 = new Ponto(6,6);
         
-        p.fill();
-        System.out.println("DISTANCIA:  " + p.distancia(p2));
+       // p.fill();
+       // System.out.println("Distancia: " + p.distancia(p, p2));
+       p.isColinear(p, p2, p3);
     }
 }
