b5b86d91016cba4f3e58ea35cbfa9a26b6471e4c
Karla C. Ferreira <karlinharural@gmail.com>
2024-03-13 19:52:21 -0300

    Update Triangulo.java

--- roteiro1/Triangulo.java
+++ roteiro1/Triangulo.java
@@ -1,66 +1,69 @@
 package com.mycompany.mavenproject3.classes;
 
-import java.util.Scanner;
-
 /**
  *
  * @author kfrural
  */
 public class Triangulo {
+
     private Ponto[] pontos = new Ponto[3];
-    
-    public Triangulo(){
-        for(int i = 0; i < 3; i++){
+
+    public Triangulo() {
+        for (int i = 0; i < 3; i++) {
             this.pontos[i] = new Ponto();
         }
     }
-    
-    public void copy(Ponto outra){
-        for(int i = 0; i < 3; i ++){
-            Ponto p = outra.getPontos()[i];
-            this.pontos[i].copy(p);
-        }
-    }
 
-    public void fill(){
-        System.out.println("Preencha os trem ai\n\n");
-        for(int i = 0; i < 3; i++){
-            this.getPontos()[i].fill();
-        }
+   public void copy(Triangulo outra) {
+    for (int i = 0; i < 3; i++) {
+        Ponto p = outra.getPontos()[i];
+        this.pontos[i].copy(p);
     }
-    
-    public void imprimir(){
+}
+
+    public void imprimir() {
         System.out.println("A impressao dos trem\n\n");
-        for(Ponto p : this.getPontos()){
+        for (Ponto p : this.getPontos()) {
             p.imprimir();
         }
     }
-    public double distancia(Ponto p1, Ponto p2){
-        return Math.sqrt(Math.pow(p2.getX() - p1.getX(), 2) + Math.pow(p2.getY() - p1.getY(), 2));
+
+    public boolean isColinear(Ponto p1, Ponto p2, Ponto p3) {
+        double a = p1.getX() * (p2.getY() - p3.getY()) + p2.getX() * (p3.getY() - p1.getY()) + p3.getX() * (p1.getY() - p2.getY());
+        return a == 0;//faz a mema coisa
+//        if (a == 0) {
+//            return true;
+//        } else {
+//           return false;
+//        }
+    }
+
+   public double calcularArea() {
+       double a = Ponto.distancia(this.pontos[0], this.pontos[1]);
+        double b = Ponto.distancia(this.pontos[1], this.pontos[2]);
+        double c = Ponto.distancia(this.pontos[2], this.pontos[0]);
+        double s = (a + b + c) / 2;
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
     }
     
-    public void isColinear(Ponto p1, Ponto p2, Ponto p3){
-        double a = p1.getX() * (p2.getY() - p3.getY()) + p2.getX() * (p3.getY() - p1.getY()) + p3.getX() *(p1.getY() - p2.getY());
-        if(a == 0){
-            System.out.println("Sim");
-        }else{
-            System.out.println("Nao");
+    public double calcularPerimetro() {
+        return Ponto.distancia(this.pontos[0], this.pontos[1]) + Ponto.distancia(this.pontos[1], this.pontos[2]) + Ponto.distancia(this.pontos[2], this.pontos[0]);
+    }
+    
+    public String verificarTipo() {
+        double a = Ponto.distancia(this.pontos[0], this.pontos[1]);
+        double b = Ponto.distancia(this.pontos[1], this.pontos[2]);
+        double c = Ponto.distancia(this.pontos[2], this.pontos[0]);
+
+        if (a == b && b == c) {
+            return "Equilateral";
+        } else if (a == b || b == c || a == c) {
+            return "Isosceles";
+        } else {
+            return "Scalene";
         }
     }
-     public double calcularArea() {
 
-    double maiorX = Math.max(Math.max(pontos[0].getX(), pontos[1].getX()), pontos[2].getX());
-    double menorX = Math.min(Math.min(pontos[0].getX(), pontos[1].getX()), pontos[2].getX());
-    double base = maiorX - menorX;
-
-    double maiorY = Math.max(Math.max(pontos[0].getY(), pontos[1].getY()), pontos[2].getY());
-    double menorY = Math.min(Math.min(pontos[0].getY(), pontos[1].getY()), pontos[2].getY());
-    double altura = maiorY - menorY;
-
-    return (base * altura) / 2;
-    }
-     
-    
     public Ponto[] getPontos() {
         return pontos;
     }
@@ -68,6 +71,5 @@ public class Triangulo {
     public void setPontos(Ponto[] pontos) {
         this.pontos = pontos;
     }
-    
-    
+
 }
