4fd7efeb978b5ed4d7176711e4576484761d29ad
Karla C. Ferreira <karlinharural@gmail.com>
2024-03-13 19:53:04 -0300

    Update main.java

--- roteiro1/main.java
+++ roteiro1/main.java
@@ -1,6 +1,8 @@
 package com.mycompany.mavenproject3;
 
 import com.mycompany.mavenproject3.classes.Ponto;
+import com.mycompany.mavenproject3.classes.Quadrado;
+import com.mycompany.mavenproject3.classes.Triangulo;
 
 /**
  *
@@ -9,13 +11,29 @@ import com.mycompany.mavenproject3.classes.Ponto;
 public class Mavenproject3 {
 
     public static void main(String[] args) {
-        
-               Ponto p = new Ponto(4, 40);
-        Ponto p2 = new Ponto(2,2);
-        Ponto p3 = new Ponto(6,6);
-        
-       // p.fill();
-       // System.out.println("Distancia: " + p.distancia(p, p2));
-       p.isColinear(p, p2, p3);
+
+        Ponto p1 = new Ponto(2, 2);
+        Ponto p2 = new Ponto(2, 4);
+        Ponto p3 = new Ponto(4, 2);
+        Ponto p4 = new Ponto(4, 4);
+
+//         p1.fill();
+//         p2.fill();
+//         System.out.println("Distancia: " + Ponto.distancia(p1, p2));
+//         Triangulo t1 = new Triangulo();
+//        t1.setPontos(new Ponto[]{p1, p2, p3});
+//        t1.imprimir();
+//        
+//        System.out.println("Area do Triangulo: " + t1.calcularArea());
+//        System.out.println("Perimetro do Triangulo: " + t1.calcularPerimetro());
+//       System.out.println("Tipo do Triangulo: " + t1.verificarTipo());
+//        
+//        Quadrado q1 = new Quadrado();
+//        q1.setPontos(new Ponto[]{p1, p2, p3, p4});
+//        q1.imprimir();
+//        
+//        System.out.println("Area do Quadrado: " + q1.calcularArea());
+//        System.out.println("Perimetro do Quadrado: " + q1.calcularPerimetro());
+//        System.out.println("Eh Quadrado? " + Quadrado.isQuadrado(p1, p2, p3, p4));
     }
 }
