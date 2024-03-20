7fe6fed7cfb013a77cec2a43841680017560dd78
Karla C. Ferreira <karlinharural@gmail.com>
2024-03-19 21:26:13 -0300

    Create Departamento.java

---
+++ semana6/ex9/classes/Departamento.java
@@ -0,0 +1,71 @@
+package com.mycompany.mavenproject3.classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author kfrural
+ */
+public class Departamento {
+    private String nomeDpto;
+    private Gerente gerente;
+    private List <Funcionario> funcionarios;
+
+    public Departamento(String nomeDpto) {
+        this.nomeDpto = nomeDpto;
+    }
+    
+    public Departamento(){
+        this.nomeDpto = "";
+        this.gerente = new Gerente();
+        this.funcionarios = new ArrayList<>();
+    }
+    
+    public Departamento(Departamento outra){
+        this.nomeDpto = outra.getNomeDpto();
+        this.gerente = outra.getGerente();
+        this.funcionarios = outra.getFuncionarios();
+    }
+    
+    public void copy(Departamento outra){
+        this.nomeDpto = outra.getNomeDpto();
+        this.gerente = outra.getGerente();
+        this.funcionarios = outra.getFuncionarios();
+    }
+    
+    public void adicionar(Funcionario funcionarios){
+            this.funcionarios.add(funcionarios);
+    }
+    
+    public void remover(Funcionario funcionarios){
+        this.funcionarios.remove(funcionarios);
+    }
+
+    public String getNomeDpto() {
+        return nomeDpto;
+    }
+
+    public void setNomeDpto(String nomeDpto) {
+        this.nomeDpto = nomeDpto;
+    }
+
+    public Gerente getGerente() {
+        return gerente;
+    }
+
+    public void setGerente(Gerente gerente) {
+        this.gerente = gerente;
+    }
+
+    public List<Funcionario> getFuncionarios() {
+        return funcionarios;
+    }
+
+    public void setFuncionarios(List<Funcionario> funcionarios) {
+        this.funcionarios = funcionarios;
+    }
+    
+    
+    
+}
